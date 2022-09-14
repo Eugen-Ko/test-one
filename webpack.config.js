72a92665fd600d7c7fbc6983592992ae9585db47
Eugen <kovalchuk.eak@gmail.com>
2022-09-14 21:38:20 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/test-one/'
+    publicPath: '/dist/'
   },
   module: {
     rules: [
